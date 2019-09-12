e319d5fe51971292468171f54acea07c86b33808
CalebSnell <calebs@maine.edu>
2019-09-12 16:30:41 -0400

    Added car class, WIP
    Created private objects for storing car info, and default car
    constructor

---
+++ A1CalebSnell/src/Car.java
@@ -0,0 +1,21 @@
+
+public class Car {
+	
+	// private objects for storing car information
+	private String cMake;
+	private String cModel;
+	private String cColor;
+	private String cIsHandicapped;
+	
+	//-----CONSTRUCTORS-----//
+	// create default car with placeholder values
+	public Car() {
+		cMake = "";
+		cModel = "";
+		cColor = "";
+		cIsHandicapped = "";
+	}
+	
+	
+	
+}
