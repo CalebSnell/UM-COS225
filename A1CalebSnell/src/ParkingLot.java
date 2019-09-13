ef8c003f894cd8d623a4aa8ca4577357401a5321
CalebSnell <calebs@maine.edu>
2019-09-13 19:33:17 -0400

    A1 - Finished assignment, TODO: deleting cars
    Completed testing class. Adjusted parkingLot to return appropriate
    values when a car is added.

--- A1CalebSnell/src/ParkingLot.java
+++ A1CalebSnell/src/ParkingLot.java
@@ -5,8 +5,7 @@ import java.util.*;
 
 public class ParkingLot {
 	
-	// Private data for parkingSpots array and object
-	//private ArrayList<ParkingSpot> parkingSpots;
+	// Private ArrayList of type parkingSpots for storing parking spots
 	private ArrayList<ParkingSpot> parkingSpots = new ArrayList<ParkingSpot>();
 	// create default parkingSpot object
 	public ParkingSpot parkingSpot;
@@ -14,50 +13,80 @@ public class ParkingLot {
 	//-----CONSTRUCTORS-----//
 	public ParkingLot () {
 		
-		//ParkingSpot parkingSpot = new ParkingSpot();
-		
-		
 		// loop through and create 20 default parkingSpots
-		for (int ct1 = 0; ct1 < 19; ct1++) {
+		for (int ct1 = 0; ct1 <= 19; ct1++) {
 			parkingSpots.add(new ParkingSpot());
 		}
 		// set 4 parkingSpots to be handicapped only
-		for (int ct2 = 0; ct2 < 3; ct2++) {
-			//parkingSpots.set(ct2, parkingSpot.setHandicappedOnly(true));
+		for (int ct2 = 0; ct2 <= 3; ct2++) {
 			parkingSpots.get(ct2).setHandicappedOnly(true);
 		}
 	}
 
 	//-----MODIFIERS-----//
-	public int addCar (String make, String model, String color, boolean isHandicapped) {
+	public int addCar (String make, String model, String color, boolean isHandicapped, int carPos) {
+	// POST: Add Car to first available spot depending on handicap status	
 		if (isHandicapped) {
 			int ct3 = 0;
-			while(!parkingSpots.get(ct3).getCarMake().equals("None") && !parkingSpots.get(ct3).getHandicappedOnly()) {
+			// only loops while current spot has a car, and isn't handicap accessible
+			while(!parkingSpots.get(ct3).getCarMake().equals("None") || !parkingSpots.get(ct3).getHandicappedOnly()) {
 				ct3++;
 				if(ct3 >= 20) {
 					return -1;
 				}
 			}
+			// set car values based off given data
 			parkingSpots.get(ct3).setCarMake(make);
 			parkingSpots.get(ct3).setCarModel(model);
 			parkingSpots.get(ct3).setCarColor(color);
 			parkingSpots.get(ct3).setCarIsHandicapped(isHandicapped);
-			return ct3;
+			// return index of car
+			carPos = ct3;
+			return carPos;
 		}
 		else {
 			int ct4 = 0;
-			while(!parkingSpots.get(ct4).getCarMake().equals("None")) {
+			// only loops while current spot is not empty OR is handicap only
+			while(!parkingSpots.get(ct4).getCarMake().equals("None") || parkingSpots.get(ct4).getHandicappedOnly()) {
 				ct4++;
 				if(ct4 >= 20) {
 					return -1;
 				}
 			}
+			// set car values based off given data
 			parkingSpots.get(ct4).setCarMake(make);
 			parkingSpots.get(ct4).setCarModel(model);
 			parkingSpots.get(ct4).setCarColor(color);
 			parkingSpots.get(ct4).setCarIsHandicapped(isHandicapped);
-			return ct4;
+			// return index of car
+			carPos = ct4;
+			return carPos;
 		}
 		
+	}
+	
+	public int deleteCar (int carIndex) {
+		return carIndex;
+		// TODO	
+		}
+	
+	//------ACCESSORS------//
+	public String toString() {
+	// POST: returns all car info concatenated into a string
+		int availHandicapSpots = 0;
+		int availStandardSpots = 0;
+		
+		// counts available handicap and standard spots
+		for (int ct5 = 0; ct5 < 19; ct5++) {
+			if (parkingSpots.get(ct5).getHandicappedOnly() && parkingSpots.get(ct5).getCarMake().equals("None")) {
+				availHandicapSpots++;
+			}
+			else if (parkingSpots.get(ct5).getCarMake().equals("None") && !parkingSpots.get(ct5).getHandicappedOnly()){
+				availStandardSpots++;
+			}
+		}
+		// returns available handicap and standard spots separated by a space
+		String availSpots = Integer.toString(availHandicapSpots) + " " + Integer.toString(availStandardSpots);
+		return availSpots;
 	}
 }
--- A1CalebSnell/src/ParkingTester.java
+++ A1CalebSnell/src/ParkingTester.java
@@ -2,12 +2,23 @@
 public class ParkingTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Create new parkingLot object
 		ParkingLot parkingLot = new ParkingLot();
-		parkingLot.addCar("ford", "fordus", "blue", false);
-		System.out.println();
+		int carPos = 0;
 		
+		// Prints parking lot availability, handicap spots + standard spots
+		System.out.printf("%s%s%n", "Handicap / Stanard Spots: ", parkingLot.toString());
+		
+		// Adds a car with given parameters
+		carPos = parkingLot.addCar("Lexus", "RX", "Blue", true, carPos);
+		System.out.printf("%s%s%n", "Handicap / Stanard Spots: ", parkingLot.toString());
+		// Prints position of previously added car
+		System.out.printf("%s%d%n","Your car is in spot: ", carPos);
+		
+		// Adds custom car
+		carPos = parkingLot.addCar("Testla", "Test S", "Silver", true, carPos);
+		System.out.printf("%s%s%n", "Handicap / Stanard Spots: ", parkingLot.toString());
+		System.out.printf("%s%d%n","Your car is in spot: ", carPos);
 	}
 
 }
