7dd12331a344770455c5ebf424dddfaaede0c897
CalebSnell <calebs@maine.edu>
2019-11-21 17:08:25 -0500

    A6 - Edited main, added treenode
    Created loop to scan morse data, finished
    Created method for inserting morse data, WIP

--- A6CalebSnell/src/A6CalebSnell.java
+++ A6CalebSnell/src/A6CalebSnell.java
@@ -1,7 +1,38 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class A6CalebSnell {
+    public static void main(String[] args) throws IOException {
+        // Constants
+        int alphabetLength = 26;
 
-    public static void main(String[] args) {
+        // Tree containing characters following morse pattern
+        TreeNode<String> morseTree = new TreeNode<String>();
 
+        // Scanner object for scanning in file input
+        Scanner fileScan = new Scanner(new FileReader("morsecode.txt"));
+
+        // Arrays for storing alphabet letters and morse encoding
+        String[] morseArray = new String[alphabetLength];
+        String[] morseChars = new String[alphabetLength];
+
+        // Loop through morse file and get alphabet characters + their respective encoding
+        for (int ct1 = 0; ct1 < alphabetLength; ct1++) {
+            morseChars[ct1] = fileScan.next();
+            morseArray[ct1] = fileScan.nextLine();
+        }
+
+        System.out.println("Suggems");
+    }
+
+    public String insertMorseChar (String[] morseChar, String[] morseArray, TreeNode<String> morseTree) {
+    // PRE: morseChar == a-z, morseArray (max size of 4) == 'o' or '-'
+    // PRE: morseTree already has empty root
+    // POST: Inserts alphabet character to tree based off morse code
+
+
+        return "sugma";
     }
 
 }
